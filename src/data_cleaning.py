6eda95c12ecaf189893db72ab1e8144890b3c91e
Sakshi-Dan-2002 <dandekarsakshi86@gmail.com>
2026-09-03 11:18:21 +0530

    Update data_cleaning

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,21 +1,7 @@
 import pandas as pd
-from pathlib import Path
 
-# Get the main project folder
-BASE_DIR = Path(__file__).resolve().parent.parent
+# Load raw sell prices data
+df = pd.read_csv("data/raw/sell_prices.csv")
 
-# File paths
-calendar_path = BASE_DIR / "data" / "raw" / "calendar.csv"
-sales_path = BASE_DIR / "data" / "raw" / "sales_train_validation.csv"
-prices_path = BASE_DIR / "data" / "raw" / "sell_prices.csv"
-
-# Load datasets
-calendar = pd.read_csv(calendar_path)
-sales = pd.read_csv(sales_path)
-prices = pd.read_csv(prices_path)
-
-print("Files loaded successfully!")
-
-print("Calendar:", calendar.shape)
-print("Sales:", sales.shape)
-print("Prices:", prices.shape)
+# Show first 5 rows
+print(df.head())
